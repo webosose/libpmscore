c4b8c344fffc3b482edbd157e0f27585ee45bcef
Punnarao D <punnarao.d@lge.com>
2019-09-19 20:24:52 +0530

    Fix power manager service shutdown issue
    :Release Notes:
    Fix power manager segmentation fault issue
    
    :Detailed Notes:
    - Fix delay in the shutdown of service when command
    'systemctl stop powermgr' executed
    - Fix segmentation fault during the service stop
    The root cause of issue is LSGmainDetach() used along with
    LSUnregister() function. Due that LsHandle corrupted,
    which in-turn causing the delay in stopping the service
    and delay in the reboot
    Trying to delete pointer two times in the destructor of
    class which causes segmentation fault for mpconfig pointer
    
    :Testing Performed:
    Tested on reference build 1819.
    test case :QR-228
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-84035] [Webos PRO] : Delayed Boot with stop job logs observed.
    
    Change-Id: I96cfebfad44ed756a9b3af407f4a88c979c713d5
    Reviewed-on: http://gpro.lge.com/256074
    Reviewed-by: Punnarao D <punnarao.d@lge.com>
    Reviewed-by: AnilKumar Reddy <anilkumar.reddy@lge.com>
    Reviewed-by: Sushovan Gangopadhy <sushovan.g@lge.com>
    Tested-by: Punnarao D <punnarao.d@lge.com>
    Reviewed-by: Rama Krishna <rama.krishna@lge.com>

--- include/public/pmscore/IpcInterfaceBase.h
+++ include/public/pmscore/IpcInterfaceBase.h
@@ -1,6 +1,6 @@
 // @@@LICENSE
 //
-//      Copyright (c) 2017 LG Electronics, Inc.
+//      Copyright (c) 2019 LG Electronics, Inc.
 //
 // Confidential computer software. Valid license from LG required for
 // possession, use or copying. Consistent with FAR 12.211 and 12.212,
@@ -31,9 +31,6 @@ public:
 
       delete mpLog;
       mpLog = nullptr;
-
-      delete mpConfig;
-      mpConfig = nullptr;
   }
 
   virtual PmsErrorCode_t Initialize ()
